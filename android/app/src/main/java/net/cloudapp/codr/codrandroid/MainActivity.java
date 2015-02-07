5217434da3e94be0d5a90f1ccdd6b504b4b58d02
jp94 <jamespark@gatech.edu>
2015-02-07 14:01:43 -0500

    Added WebView & Full Screen
    commit #2
    WebView includes: url loading and default web view client

--- android/app/src/main/java/net/cloudapp/codr/codrandroid/MainActivity.java
+++ android/app/src/main/java/net/cloudapp/codr/codrandroid/MainActivity.java
@@ -5,17 +5,30 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Remove title bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        /* WEB VIEW ---------------------------------------------------------------------------*/
+        // Initialize and attach id
+        webView = (WebView) findViewById(R.id.activity_main_webview);
+        // Enable JavaScript
+        webView.getSettings().setJavaScriptEnabled(true);
+        // Load main web page
+        webView.loadUrl("http://codr.cloudapp.net");
+        // Force links and redirects to open in the app, instead of using an external browser app
+        webView.setWebViewClient(new WebViewClient());
+
     }
 
 
