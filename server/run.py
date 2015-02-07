170910755ec14137ee385cdbf6ae1cb743f76a40
Creston <creston@bunch.im>
2015-02-07 12:42:04 -0500

    this might fix everything

--- server/run.py
+++ server/run.py
@@ -11,10 +11,10 @@ import config
 application = tornado.web.Application([
     (r"/api/login", api.login.Handler),
     (r"/api/user/(?P<uid>[^\/]+)/?", api.user.UserHandler),
-    (r"/api/like/(?P<source_id>[^\/]+)/(?P<target_id>[^\/]+)/?", api.user.LikeHandler),
-    (r"/api/reject/(?P<source_id>[^\/]+)/(?P<target_id>[^\/]+)/?", api.user.RejectHandler),
-    (r"/api/find/(?P<uid>[^\/]+)/?", api.user.FindHandler),
-    (r"/api/matches/(?P<uid>[^\/]+)/?", api.user.MatchesHandler)
+    (r"/api/like/(?P<target_id>[^\/]+)/?", api.user.LikeHandler),
+    (r"/api/reject/(?P<target_id>[^\/]+)/?", api.user.RejectHandler),
+    (r"/api/find/?", api.user.FindHandler),
+    (r"/api/matches/?", api.user.MatchesHandler)
 ], cookie_secret=config.app_secret)
 
 if __name__ == "__main__":
