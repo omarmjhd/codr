2bb50dd8e6d5a687b28db4cf7625b317bb7499bb
Creston <creston@bunch.im>
2015-02-07 02:59:12 -0500

    Cusuh level commit messege: aoeaosnethaonethaoentaohe

--- server/api/login.py
+++ server/api/login.py
@@ -33,7 +33,30 @@ class Handler(tornado.web.RequestHandler):
             pass
 
 
-
     def get(self):
-        self.write("Hello, world")
+        # code provided by github
+        code = self.get_argument('code')
+        post_args = {'code': code,
+                     'client_id': config.gh_id,
+                     'client_secret': config.gh_secret}
+
+        # request some shit from github
+        http_client = httpclient.HTTPClient()
+        try:
+            response = json.loads(http_client.fetch(
+                config.gh_ex_url,
+                method='POST',
+                body=urllib.urlencode(post_args),
+                headers={'Accept':'application/json'}
+            ))
+
+            print(response)
+        except httpclient.HTTPError as e:
+            # HTTPError is raised for non-200 responses; the response
+            # can be found in e.response.
+            pass
+        except Exception as e:
+            # Other errors are possible, such as IOError.
+            pass
+
 
