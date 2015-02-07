5cfcd576446d5cc6dcc8983428a2a3c579b81ce2
Creston <creston@bunch.im>
2015-02-07 01:40:58 -0500

    Why was I missing files? #nosleep

---
+++ server/config.py
@@ -0,0 +1,7 @@
+# GitHub API access tokens
+gh_id = 'be249dd939fe1ef1aa3d'
+gh_secret = '867a9fe4309e9a0b4a442ffb3d866d67970977e8'
+# GitHub API exchange URL
+gh_ex_url = 'https://github.com/login/oauth/access_token'
+# GitHub API endpoint URL
+gh_ep_url = 'https://api.github.com'
