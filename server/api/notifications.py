09efaa9765c015d2f1e49274a096831635e67716
Creston <creston@bunch.im>
2015-02-07 17:58:41 -0500

    ws test

--- server/api/notifications.py
+++ server/api/notifications.py
@@ -4,13 +4,12 @@ from models import users
 class NotificationsWebSocket(tornado.websocket.WebSocketHandler):
 
     def open(self):
-        #user = users.get_user(int(self.get_secure_cookie("user")))
+        user = users.get_user(int(self.get_secure_cookie("user")))
         print('Websockets connected.')
-        self.write('test')
 
     def on_message(self, message):
-        print('test')
-        self.write_message(message)
+        print('MATCH ------------')
+        print(message)
 
     def on_close(self):
         pass
