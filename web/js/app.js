d80ebe60b2750c0e4c9bbb42598cae1d33a403da
Creston <creston@bunch.im>
2015-02-07 15:56:42 -0500

    HOTFIX MY NIX

--- web/js/app.js
+++ web/js/app.js
@@ -43,6 +43,7 @@ angular.module('codr', ['ngRoute'])
         $http.get('/api/find')
         .then(function(result) {
             $scope.person = result.data;
+            if ($scope.person) {
                 var languages = '';
                 var keys = Object.keys($scope.person.languages);
                 for (l in keys) {
@@ -51,8 +52,9 @@ angular.module('codr', ['ngRoute'])
                 $scope.person.languages = languages.substring(
                     0, languages.length - 2);
 
-            // update sample snippet
-            $scope.sampleSnippet();
+                // update sample snippet
+                $scope.sampleSnippet();
+            }
         });
     };
 
