46784bf571056a373951ba4f5548c1b6214b0110
Creston <creston@bunch.im>
2015-02-07 14:18:27 -0500

    fix

--- server/run.py
+++ server/run.py
@@ -10,21 +10,21 @@ import api.user
 
 import config
 
-application = tornado.web.Application([
-    (r"/api/login", api.login.Handler),
-    (r"/api/user/(?P<uid>[^\/]+)/?", api.user.UserHandler),
-    (r"/api/like/(?P<target_id>[^\/]+)/?", api.user.LikeHandler),
-    (r"/api/reject/(?P<target_id>[^\/]+)/?", api.user.RejectHandler),
-    (r"/api/find/?", api.user.FindHandler),
-    (r"/api/matches/?", api.user.MatchesHandler)
-], cookie_secret=config.app_secret)
-
 if __name__ == "__main__":
 
     if sys.argv[1] and sys.argv[2] and sys.argv[3]:
         config.gh_id = sys.argv[1]
         config.gh_secret = sys.argv[2]
         config.app_secret = sys.argv[3]
+
+    application = tornado.web.Application([
+        (r"/api/login", api.login.Handler),
+        (r"/api/user/(?P<uid>[^\/]+)/?", api.user.UserHandler),
+        (r"/api/like/(?P<target_id>[^\/]+)/?", api.user.LikeHandler),
+        (r"/api/reject/(?P<target_id>[^\/]+)/?", api.user.RejectHandler),
+        (r"/api/find/?", api.user.FindHandler),
+        (r"/api/matches/?", api.user.MatchesHandler)
+    ], cookie_secret=config.app_secret)
 
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
