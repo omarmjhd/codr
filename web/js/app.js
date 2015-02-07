ff2e8675d0b9d0cbd274ccf67e5552f3dad7de3c
Cusuh <cusuh@gatech.edu>
2015-02-07 03:23:30 -0500

    ........................................... :(

---
+++ web/js/app.js
@@ -0,0 +1,9 @@
+angular.module('codr', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'templates/login.html',
+            controller: 'loginCtrl'
+        });
+}]);
