4e48cc57519d554a40914f8b0a9f160b2b0edd42
Creston <creston@bunch.im>
2015-02-07 18:00:13 -0500

    ws test

--- web/js/app.js
+++ web/js/app.js
@@ -23,7 +23,7 @@ angular.module('codr', ['ngRoute'])
         .then(function(result) {
             $scope.matched = result.data;
             if ($scope.matched === 'true') {
-                ws.send($scope.person._id)
+                ws.send($scope.person._id);
                 alert('You matched!');
             }
             // find a new person
