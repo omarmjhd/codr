8ef7f955860823b1d1c37cadd1619e905e23bdeb
Omar Mujahid <omar_mjhd@yahoo.com>
2015-02-07 17:52:13 -0500

    xcode project made that has a sample iOS Ported version of our App

---
+++ ios/codr/codr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  codr
+//
+//  Created by Omar Mujahid on 2/7/15.
+//  Copyright (c) 2015 Omar Mujahid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
