ceffe2f677f506c7c7e4ada946fb31df34e9217f
Creston <creston@bunch.im>
2015-02-08 02:34:56 -0500

    test

--- server/api/chat.py
+++ server/api/chat.py
@@ -10,26 +10,25 @@ class ChatWebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
         self.user = int(self.get_secure_cookie("user"))
         chatters[self.user] = self
-        self.target = None
         print('Chat connected.')
 
     def on_message(self, e):
         e = json.loads(e)
         # check all connections and notify the other matched user
-        self.target = int(e['target'])
+        target = int(e['target'])
         msg = e['msg']
         matches =  [x['id'] for x in users.get_matches(self.user)]
         # target is chatting and is a match
-        if self.target in chatters and self.target in matches:
+        if target in chatters and target in matches:
             print(e)
             author = users.get_user(self.user)
-            chatters[self.target].write_message(author['name'] + ': ' + msg)
+            chatters[target].write_message(author['name'] + ': ' + msg)
             self.write_message('You: ' + msg)
 
         # notify the match for a chat
         else:
             for n in notifiers:
-                if n.user == self.target:
+                if n.user == target:
                     user = users.get_user(self.user)
                     self.write_message('You: ' + msg)
                     n.write_message(user['name'])
