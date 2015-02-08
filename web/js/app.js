b4eb88baccd74e46c5411bbc0a2dd3dd911b8a6a
Creston <creston@bunch.im>
2015-02-08 00:57:04 -0500

    fix

--- web/js/app.js
+++ web/js/app.js
@@ -161,8 +161,8 @@ angular.module('codr', ['ngRoute'])
     }
 
     $scope.send = function() {
-        console.log(uid);
-        console.log($scope.userMsg);
-        chat_ws.send(angular.toJson({'target': uid, 'msg' : $scope.userMsg}));
+        chat_ws.send(angular.toJson(
+            {'target': uid.toString(), 'msg' : $scope.userMsg})
+        );
     }
 }]);
