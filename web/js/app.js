f887a443077d619dd00881207e6e180f15eb7567
jp94 <jamespark@gatech.edu>
2015-02-08 01:31:25 -0500

    Revert "Re attempt prettyprint fix"
    This reverts commit ad232d847c8df7366dc70d949f151fbae0c85642.

--- web/js/app.js
+++ web/js/app.js
@@ -185,12 +185,3 @@ angular.module('codr', ['ngRoute'])
         });
     };
 });
-
-.directive('prettyprint', function() {
-    return {
-        restrict: 'C',
-        link: function postLink(scope, element, attrs) {
-              element.html(prettyPrintOne(scope.dom));
-        }
-    };
-});
