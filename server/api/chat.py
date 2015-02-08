721d8e9da246fa1ebaba9a1ec21a6606ac0a6d43
Creston <creston@bunch.im>
2015-02-07 23:56:58 -0500

    fix

--- server/api/chat.py
+++ server/api/chat.py
@@ -17,7 +17,7 @@ class ChatWebSocket(tornado.websocket.WebSocketHandler):
         # check all connections and notify the other matched user
         target = e['target']
         msg = e['msg']
-        matches = users.matches(self.user)
+        matches = users.get_matches(self.user)
         # target is chatting and is a match
         if target in chatters and chatters[target].user in matches:
             chatters[target].write_message('Match: ' + msg)
